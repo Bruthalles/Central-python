13d4e88a08e447ab1a1f903549f135d37bdf25fe
Bruthalles <thallesgmkr@gmail.com>
2025-03-04 17:21:35 -0300

    teste de booleano

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-if (5 > 2 ):
-    print("oi");
+""" class Cliente{
+
+} """
+
+
+
+def boolear():
+    num1 = input(print("\n digite numero 1:"))
+    num2 = input(print("\n digite numero 2:"))
+
+    teste = num1 > num2
+    
+    print(f"{num1} é maior que {num2} ? {teste}")
+
+
+if __name__ == "__main__":
+    boolear()
+    
+        
