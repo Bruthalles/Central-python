2934c21ae5b58ae2d513760618bd12d1e1ec6ab8
Bruthalles <thallesgmkr@gmail.com>
2025-03-12 16:34:07 -0300

    funçoes da conta finalizadas e senha escondida

--- classes/ContaBancaria.py
+++ classes/ContaBancaria.py
@@ -1,29 +1,23 @@
-""" Crie uma classe ContaBancaria com:
-
-Um atributo privado saldo.
-Métodos depositar(valor) e sacar(valor), verificando se há saldo suficiente.
-Desafio extra:
-Adicione um método exibir_saldo() que exibe o saldo apenas se for chamado de dentro da classe. """
-
 class Contabancaria:
     def __init__(self,balance, valor):
-        self.__balance = balance
-        self.valor = valor
+        self.__balance = float(balance)
+        self.valor = float(valor)
 
     def fill(self,valor):
 
-        if(valor >0):
-            self.__balance += valor
-            print("\nR${valor} depositado!")
+        if(float(valor) >0):
+            self.__balance += float(valor)
+            print(f"\nR${valor} depositado!")
 
         else: print("\nValor inválido para depósito")
 
     def empty(self,valor):
-        if(0<valor<=self.__balance):
-            self.__balance -= valor
+
+        if(0<float(valor)<=self.__balance):
+            self.__balance -= float(valor)
             print(f"\nSaque de R${valor} efetuado!")
 
         else: print("\nSaldo insuficiente ou inválido")
 
     def show_balance(self):
-        print(f"R${self.__balance}")
+        print(f"\nSeu saldo atual é: R${self.__balance}")
--- main.py
+++ main.py
@@ -1,59 +1,56 @@
-import boolear
-from classes.Carro import Carro
+import getpass
 from classes import Cadastro
 from classes import ContaBancaria
 
 usr = Cadastro.Usuario()
 count = ContaBancaria.Contabancaria(0,0)
 
-def menu():
-    print(f"=======Bem-vindo, {usr.name}=======")
-    print("\nDigite 1 para ver saldo |")
-    print("\nDigite 2 para depositar |")
-    result = int(input("\nDigite 3 para sacar: "))
+def option():
+    result = int(input("\nEscolha uma ação: "))
 
     if(result== 1):
         count.show_balance()
-        menu()
+        option()
 
     elif(result==2):
         valor = input("\nQuanto irá depositar? ")
         count.fill(valor)
-        menu()
+        option()
 
     elif(result==3):
         valor = input("\nDigite valor para saque: ")
         count.empty(valor)
-        menu()
+        option()
 
     else: 
         print("\nOPÇÃO INDISPONÍVEL!")
-        menu()
+        option()
+
+def menu():
+    
+    print("\nDigite 1 para ver saldo |")
+    print("\nDigite 2 para depositar |")
+    print("\nDigite 3 para resgatar  |")
+    option()
 
 def login():
 
     check_name = input("\nInsira nome de usuário: ")
-    check_passw = input("\nInsira sua senha: ")
+    check_passw = getpass.getpass("\nInsira sua senha: ")
 
     if(check_name == usr.name and check_passw == usr.passw):
+        print(f"\n=======Bem-vindo, {usr.name}=======")
         menu()
 
     else: print("\nNOME DE USUÁRIO OU SENHA INCORRETOS")
-
 
 if __name__ == "__main__":
     if (usr.name == None):
         usr.name = input("\nCadastre um nome de usuário para entrar na conta: ")
 
     if(usr.passw == None):
-        usr.passw = input("\nCadastre sua senha para passar: ")
+        usr.passw = getpass.getpass("\nCadastre sua senha para passar: ")
         login()
 
-    
     else:
         login()
-
-    
-    
-
-        
