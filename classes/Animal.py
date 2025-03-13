d272725b3dd7750c1d8ffe21fb6e0f1669745488
Bruthalles <thallesgmkr@gmail.com>
2025-03-13 14:50:46 -0300

    Ex.Animal dividido e finalizado

---
+++ classes/Animal.py
@@ -0,0 +1,22 @@
+'''Crie uma classe Animal com um método fazer_som(). Depois, crie duas classes Cachorro e Gato que herdam de Animal e sobrescrevem fazer_som() para retornar "Au Au!" e "Miau!", respectivamente.
+
+Desafio extra:
+Crie uma lista de animais (cães e gatos misturados) e percorra a lista chamando fazer_som().'''
+
+#definindo tudo o que será exportado
+__all__ = ["Cat","Dog"]
+
+class Animal:
+    
+    def __init__(self,name):
+        self.name = name
+
+    def fazer_som(str):
+        print(str)
+        return 0 
+
+class Cat(Animal):
+    Animal.fazer_som("MIAAAAAAAAAU")
+
+class Dog(Animal):
+    Animal.fazer_som("AUUUUUUUUUUUU")
---
+++ programs/animals.py
@@ -0,0 +1,28 @@
+from classes.Animal import Cat,Dog
+animals = ["","","","",""]
+
+cat = Cat("")
+dog = Dog("")
+
+def catordog():
+
+    pet = input("\n Cat or Dog ? ")
+    pet = pet.lower()
+   
+    if(pet == "cat"):
+        print(f"\nOlha o meme gritando miau. MIAAAAAAAAAAAAU")
+    
+    elif(pet == "dog"):
+        print("\nAUUUUUUUUUUUU")
+        print("Baby, I'm preyin' on you tonight")
+        print("Hunt you down, eat you alive")
+        print("Just like animals, animals")
+        print("Like Animals-mals")
+
+    else:
+        print(f"Por acaso você não sabe escrever ou só tá me testando?")
+        print(f"Isso aqui não é bagunça, ou você me obedece ou cai fora >:(")
+    
+def Pg_animals():
+    Cat.fazer_som(catordog())
+    
