c905c11dd86e11f18e4a68660bec745f6b607196
Bruthalles <thallesgmkr@gmail.com>
2025-03-13 14:52:49 -0300

    Ex.Endereço criado e organizando iniciação

---
+++ classes/Relationship.py
@@ -0,0 +1,6 @@
+'''Exercício:
+Crie uma classe Pessoa com nome e idade. Depois, crie uma classe Endereco com rua e cidade.
+A classe Pessoa deve ter um atributo endereco que recebe um objeto da classe Endereco.
+
+Desafio extra:
+Permita que a Pessoa tenha mais de um endereço e imprima todos.'''
