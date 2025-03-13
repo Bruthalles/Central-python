615caab1d287a93dfd16234345c18eb372975572
Bruthalles <thallesgmkr@gmail.com>
2025-03-13 14:47:39 -0300

    Ex.Car dividido e finalizado

---
+++ classes/Car.py
@@ -0,0 +1,15 @@
+class Car:
+    
+    def __init__(self,marca= "nulo",modelo="nulo",ano=0):
+        self.marca = marca 
+        self.modelo = modelo
+        self.ano = ano
+
+    def set_info(self):
+        self.marca = input("Marca: ")
+        self.modelo = input("Modelo: ")
+        self.ano = input("Ano: ")
+
+    def show_info(self):
+        print("\ninformaçoes do seu carro:")
+        print(f"{self.marca} {self.modelo} {self.ano}")
---
+++ programs/car.py
@@ -0,0 +1,7 @@
+from classes.Car import Car
+
+def Pg_Car():
+    car = Car()
+
+    car.set_info()
+    car.show_info()
