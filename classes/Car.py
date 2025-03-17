799245a61da49b9e1630adeaefd2f601fa12d032
Bruthalles <thallesgmkr@gmail.com>
2025-03-17 14:37:38 -0300

    added list of cars

--- classes/Car.py
+++ classes/Car.py
@@ -3,19 +3,22 @@ Crie uma classe Carro com os atributos marca, modelo e ano. Adicione um método 
 
 Desafio extra:
 Crie um objeto da classe Carro e chame o método exibir_detalhes().'''
-
 class Car:
     
     def __init__(self,marca= "nulo",modelo="nulo",ano=0):
         self.marca = marca 
         self.modelo = modelo
         self.ano = ano
+        self.cars = []
 
     def set_info(self):
-        self.marca = input("Marca: ")
-        self.modelo = input("Modelo: ")
+        self.marca = input("Marca: ").capitalize()
+        self.modelo = input("Modelo: ").capitalize()
         self.ano = input("Ano: ")
-
-    def show_info(self):
-        print("\ninformaçoes do seu carro:")
-        print(f"{self.marca} {self.modelo} {self.ano}")
+        
+    def add_car(self,objcar):
+        self.cars.append(objcar)
+        print("\nSua garagem:")
+        for i in self.cars:
+            print(f"\n{i}")
+        return 0
