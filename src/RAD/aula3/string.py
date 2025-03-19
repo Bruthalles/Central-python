2290a88dc1c957fe223d1310ee821aaaf75fcbea
THALLES ROBERTO CHOTE BRUMATTI <202302757596@alunos.estacio.br>
2025-03-19 20:27:27 -0300

    organizado e string.py manipulando strings

---
+++ src/RAD/aula3/string.py
@@ -0,0 +1,30 @@
+texto = "Nossa primeira aula manipulando string."
+
+'''print(texto[0:20:2])
+print(len(texto))
+print(texto.count("a"))
+print(texto.count("a",5,36))
+print(texto.find("aula"))
+print(texto.find("Arroz"))
+print("String"in texto)
+print("a"in texto)
+
+novo_texto = texto.replace("manipulando","trabalhando com")
+print(novo_texto)
+
+print(texto.startswith("Nossa"))
+print(texto.startswith("aula"))
+print(texto.endswith("aula"))
+print(texto.endswith("."))
+
+print(texto.lower())
+print(texto.upper())
+print(texto.capitalize())
+print(texto.title())
+print(texto.swapcase())'''
+
+nome = str(input("digite nome: "))
+print(f"Olá,{nome}!")
+print(f"Olá,{nome.strip()}!")
+#rstrip() limpa espaço na direita
+#lstrip() limpa espaço na esquerda
