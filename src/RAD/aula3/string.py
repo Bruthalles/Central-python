fa096c36aba7bc25c426d6195eba90323a1211e3
THALLES ROBERTO CHOTE BRUMATTI <202302757596@alunos.estacio.br>
2025-03-19 21:18:45 -0300

    mais funcoes e regex no string.py

--- src/RAD/aula3/string.py
+++ src/RAD/aula3/string.py
@@ -1,6 +1,7 @@
-texto = "Nossa primeira aula manipulando string."
+import re
 
-'''print(texto[0:20:2])
+'''texto = "Nossa primeira aula manipulando string."
+print(texto[0:20:2])
 print(len(texto))
 print(texto.count("a"))
 print(texto.count("a",5,36))
@@ -23,8 +24,45 @@ print(texto.capitalize())
 print(texto.title())
 print(texto.swapcase())'''
 
-nome = str(input("digite nome: "))
+'''nome = str(input("digite nome: "))
 print(f"Olá,{nome}!")
 print(f"Olá,{nome.strip()}!")
 #rstrip() limpa espaço na direita
 #lstrip() limpa espaço na esquerda
+
+print(texto)
+print(f"texto cortado: {texto.split()}")
+print(f"espaçio inserido: {''.join(texto)}")
+print(f"texto cortado: {texto.split()}")
+print(f"texto cortado e com espaço: {''.join(texto.split())}")
+
+def get_coxinhas(*pedidos):
+    return [f'{pedido} coxinhas'for pedido in pedidos]
+
+salgados_return = get_coxinhas(4,6,8)
+print("usando return")
+print(salgados_return)
+
+def get_joelho(*pedidos):
+    for pedido in pedidos:
+        yield f"{pedido} joelho(s)"
+
+print("\nusando yield")
+for salgado in get_joelho(4,6,8):
+    print(salgado)'''
+
+texto = "o numero de telefone é (123) 456-7890"
+padrao = r'\(\d{3}\) \d{3}-\d{3}'
+
+resultado = re.search(padrao,texto)
+
+if resultado:
+    numero_telefone = resultado.group()
+    print("numero de telefone encontrado",numero_telefone)
+else: print("numero nao encontrado")
+
+texto = "meu email é exemplo@gmail.com"
+padrao = r'\b\w+@+\w+\.\w+b'
+
+novotexto = re.sub(padrao,"[email oculto]",texto)
+print(novotexto)
