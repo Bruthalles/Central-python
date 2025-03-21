feb38fabfdd7b8258fcc46bdd68b03dddeaf843c
Bruthalles <thallesgmkr@gmail.com>
2025-03-21 19:09:25 -0300

    finished first version server

--- app.py
+++ app.py
@@ -1,30 +1,52 @@
-from flask import Flask, render_template
+import time
+from flask import Flask,request, render_template, send_from_directory
+from backend.programs.animals import catordog
+app = Flask(__name__,template_folder="src/",static_folder="src/static")
 
-app = Flask(__name__,template_folder="src/pages")
-
+#///////////////////////  ROUTES  ///////////////////////////////      
 @app.route('/')
 def home():
-    return render_template('home/index.html')
+    timestamp = time.time()
+    return render_template('pages/home/index.html',timestamp=timestamp)
 
 @app.route('/animals')
 def animal():
-    return render_template('animals/index.html')
+    timestamp = time.time()
+    return render_template('pages/animals/index.html',timestamp=timestamp)
 
 @app.route('/banco')
 def banco():
-    return render_template('bank/index.html')
+    timestamp = time.time()
+    return render_template('pages/bank/index.html',timestamp=timestamp)
 
 @app.route('/garage')
 def cars():
-    return render_template('cars/index.html')
+    timestamp = time.time()
+    return render_template('pages/cars/index.html',timestamp=timestamp)
 
 @app.route('/locations')
 def loc():
-    return render_template('elocations/index.html')
+    timestamp = time.time()
+    return render_template('pages/elocations/index.html',timestamp=timestamp)
 
 @app.route('/conversor')
 def conversor():
-    return render_template("etemperature/index.html")
+    timestamp = time.time()
+    return render_template("pages/etemperature/index.html",)
+
+#obtendo resposta do form para verificação
+@app.route('/animals',methods=['GET','POST'])
+def processarcatordog():
+    resultado = request.form.get('catordog').lower()
+    return render_template('pages/animals/index.html',resultado=resultado)
+
+@app.route('/src/css/')
+def serve_pages_css(filename):
+    return send_from_directory(filename)
+
+@app.route('/src/assets/')
+def serve_assets(filename):
+    return send_from_directory(filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
