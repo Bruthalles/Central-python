910345f0e6fd6f7e5537435c71ed36d484e84d61
Bruthalles <thallesgmkr@gmail.com>
2025-03-24 12:07:43 -0300

    added conversions to kelvin

--- backend/classes/ConversorTemp.py
+++ backend/classes/ConversorTemp.py
@@ -6,6 +6,17 @@ Um método estático fahrenheit_para_celsius(fahrenheit).
 Desafio extra:
 Chame esses métodos sem instanciar a classe.'''
 class ConversorTemp:
+
+    @staticmethod
+    def celsius_to_kelvin(celsius):
+        kelvin = celsius + 273.15
+        return f"\n{celsius}°C = {kelvin}°K"
+    
+    @staticmethod
+    def kevil_to_celsius(kelvin):
+        celsius = kelvin - 273.15
+        return f"\n{kelvin}°K = {celsius}°C"
+    
     @staticmethod 
     def celsius_to_fahrenheit(celsius):
         fahrenheit = (celsius * 1.8) + 32
@@ -13,5 +24,15 @@ class ConversorTemp:
     
     @staticmethod
     def fahrenheit_to_celsius(fahrenheit):
-        celsius = (fahrenheit -32) *5 / 9
+        celsius = (fahrenheit -32) * 5 / 9
         return f"\n{round(fahrenheit,2)}°F = {round(celsius,2)}°C"
+    
+    @staticmethod
+    def fahrenheit_to_kelvin(fahr):
+        kelvin = (fahr +459.67) * 5 / 9
+        return f"\n{fahr}°F = {round(kelvin,4)}°K"
+    
+    @staticmethod
+    def kelvin_to_fahrenheit(kelvin):
+        fahr = kelvin * 9 / 5 - 459.67
+        return f"\n{kelvin}°K = {round(fahr,3)}°F"
--- backend/programs/temperature.py
+++ backend/programs/temperature.py
@@ -1,21 +1,61 @@
-from classes.ConversorTemp import *
-from src.modules.timer import tm
+from backend.classes.ConversorTemp import *
+from backend.modules.timer import tm
 
 def Pg_temp():
     print(f"\nExecutando Programa Conversor de temperatura... ")
     tm(1.0)
 
-    choose = input(f"\n Escolha 'C' para Celsius ou 'F' para Fahrenheit: ").upper()
+    choose = input(f"\n Escolha valor para converter, 'C' para Celsius, 'K' para Kelvin ou 'F' para Fahrenheit: ").upper()
     if (choose == "C"):
-        celsius = float(input("\nDigite o grau em celsius para retornar fahrenheit: "))
-        tm(1.5)
-        print(ConversorTemp.celsius_to_fahrenheit(celsius))
-        tm(1.2)
+        twoose = input("\nAgora escolha K ou F: ").upper()
+        if (twoose == "K"):
+            celsius = float(input("\nDigite o grau em celsius para retornar em Kelvin: "))
+            tm(1.5)
+            print(ConversorTemp.celsius_to_kelvin(celsius))
+            tm(1.2)
+        elif(twoose == "F"):
+            celsius = float(input("\nDigite o grau em celsius para retornar em Fahrenheit: "))
+            tm(1.5)
+            print(ConversorTemp.celsius_to_fahrenheit(celsius))
+            tm(1.2)
+        else:
+            print("\nopção inválida")
+            tm(1.4)
+            return 0
+
     elif(choose == "F"):
-        fahr = float(input("\nDigite o grau em fahrenheit para retornar celsius: "))
-        tm(1.5)
-        print(ConversorTemp.fahrenheit_to_celsius(fahr))
-        tm(1.2)
+        twoose = input("\n Agora escolha C ou K: ").upper()
+        if (twoose == "C"):
+            fahr = float(input("\nDigite o grau em fahrenheit para retornar celsius: "))
+            tm(1.5)
+            print(ConversorTemp.fahrenheit_to_celsius(fahr))
+            tm(1.2)
+        elif(twoose == "K"):
+            fahr = float(input("\nDigite o grau em fahrenheit para retornar em kelvin: "))
+            tm(1.5)
+            print(ConversorTemp.fahrenheit_to_kelvin(fahr))
+            tm(1.2)
+        else:
+            print("\nopção inválida")
+            tm(1.4)
+            return 0
+
+    elif(choose == "K"):
+        twoose = input("\nAgora escolha C ou f: ").upper()
+        if(twoose == "C"):
+            kelvin = float(input("\nDigite o grau em kelvin para retornar em celsius: "))
+            tm(1.5)
+            print(ConversorTemp.kevil_to_celsius(kelvin))
+            tm(1.2)
+        elif(twoose == "F"):
+            kelvin = float(input("\nDigite o grau em kelvin para retornar em fahrenheit: "))
+            tm(1.5)
+            print(ConversorTemp.kelvin_to_fahrenheit(kelvin))
+            tm(1.2)
+        else:
+            print("\nopção inválida")
+            tm(1.4)
+            return 0
     else:
         print("\nopção inválida")
         tm(1.4)
