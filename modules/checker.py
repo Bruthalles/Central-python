20365b077c091d89fd145eb65a5b711c903c1417
Bruthalles <thallesgmkr@gmail.com>
2025-03-25 21:30:58 -0300

    deleting needless files

--- modules/checker.py
+++
@@ -1,6 +0,0 @@
-def verify(print,function,elseprint):
-    choose = input(f"\n{print}").upper()
-    if(choose == "S"):
-        function()
-    elif(choose == "N"): return 0
-    else: print(f"\n{elseprint}")
--- src/modules/timer.py
+++
@@ -1,4 +0,0 @@
-import time
-
-def tm(num): 
-    time.sleep(num)
