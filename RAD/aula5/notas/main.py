99752d24fbd9e9367d265f8cb5b139ed1b2602e7
Bruthalles <thallesgmkr@gmail.com>
2025-03-28 16:27:06 -0300

    renamed notas.py for main.py

---
+++ RAD/aula5/notas/main.py
@@ -0,0 +1,15 @@
+'''voce é um professor e deseja guardar registro de notas dos alunos. pode usar um arquivo de texto para armazenar as notas de cada aluno'''
+import os
+from make_classroom import make_classroom
+from Aluno import Aluno
+
+aluno = Aluno('','','oi')
+if __name__ == '__main__':
+
+    print(aluno.turma)
+    make_classroom(aluno.nome,aluno.nota, aluno.turma)
+    
+
+
+
+        
