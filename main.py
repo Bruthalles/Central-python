d59759a463a932d9dd169cac5cc812787440a7e7
Bruthalles <thallesgmkr@gmail.com>
2025-04-01 20:51:06 -0300

    testing main

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import RAD.aula3.manip
-from RAD.aula4 import verify_email
+from RAD.aula3.manip import *
+from RAD.aula4 import *
 from RAD.aula5 import *
 
 if __name__ == '__main__':
-   email = input("\n Digite email: ")
-   verify_email(email)
+   #email = input("\n Digite email: ")
+   #verify_email(email)
+   write()
+   clean()
