7d3b7bd524b3c376e27d20bd3c6019bd9d00dfd2
Bruthalles <thallesgmkr@gmail.com>
2025-04-04 20:45:05 -0300

    created function to open form of the car

---
+++ src/static/scripts/formCar.js
@@ -0,0 +1,24 @@
+export function formCar(){
+    let container = document.getElementById("add-car-container");
+        container.innerHTML= `
+       
+            <label class="label">
+                <p class="p">Marca</p>    
+            </label>
+            <input id="marca" type="text" placeholder="Ex. Honda" required>
+
+            <label class="label">
+                <p class="p">Modelo</p>    
+            </label>
+            <input id="modelo" type="text" placeholder="Ex. Civic" required>
+
+            <label class="label">
+                <p class="p">Ano</p>    
+            </label>
+            <input id="ano"  type="text" placeholder="Ex. 2025" required>
+
+            <button class="guardar-carro" onclick="guardarCar()" type="submit">Guardar carro</button>
+       `;
+        
+}
+window.formCar = formCar;
