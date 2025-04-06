c8177701b980f9ca92d5ada819a45157abbcaf8b
Bruthalles <thallesgmkr@gmail.com>
2025-04-06 13:54:00 -0300

    added function sobrescrever

--- RAD/ex.exportar_notas/export_notes.py
+++ RAD/ex.exportar_notas/export_notes.py
@@ -1,12 +1,17 @@
 from Aluno import *
-import os
-
+import os,time
+from up_down_timer import up_down_timer
+tm = time.sleep
 def export_notes(turma):
 
     choose = input("\nX para exportar notas ou A para adicionar aluno: ").lower()
     if choose == "x":
         path = 'notas.txt'
         if not os.path.exists(path):
+
+            with open(path,'w',encoding='utf-8')as f:
+                f.write('')
+            tm(5.0)
             for aluno in turma:
                 with open(path,'a',encoding='utf-8') as file:
                     file.writelines(f"Aluno: {aluno}\n")
@@ -15,6 +20,29 @@ def export_notes(turma):
             return 0
         else:
             print(FileExistsError , f"arquivo {path} já existente")
+            twoose = int(input("\nDigite 1 para adicionar mesmo assim ou digite 2 para sobrescrever: "))
+            if twoose == 1:
+                tm(5)
+                for aluno in turma:
+                    with open(path,'a',encoding='utf-8') as file:
+                        file.writelines(f"Aluno: {aluno}\n")
+            
+                print("novo aluno adicionado à turma")
+                return 0
+            elif twoose ==2:
+                print("ARQUIVO  notas.txt SERÁ SOBRESCREVIDO EM 10 SEGUNDOS:")
+                up_down_timer()
+                tm(5)
+
+                with open(path,'w') as w:
+                    w.write('')
+                for aluno in turma:
+                    with open(path,'a',encoding='utf-8') as file:
+                        file.writelines(f"Aluno: {aluno}\n")
+            
+                print("notas.txt sobrescrevido com a nova turma")
+                return 0
+            else: print("\nOpção invalida")
     
     elif choose == "a":
         return 0
