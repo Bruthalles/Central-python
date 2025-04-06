0ec25c1bfbb0ec91e86ae5ac54ba26234973e9bf
Bruthalles <thallesgmkr@gmail.com>
2025-04-06 14:12:31 -0300

    created class Contato

---
+++ RAD/ex.agenda_contatos/Contato.py
@@ -0,0 +1,8 @@
+class Contato:
+    def __init__(self,nome,sobrenome,telfone,email)->str:
+        self.nome =nome
+        self.sobrenome = sobrenome
+        self.telefone = telfone
+        self.email = email
+        
+        
