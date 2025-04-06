5651274010f5e0293e6fb7db374edd6a1798ffa4
Bruthalles <thallesgmkr@gmail.com>
2025-04-06 14:13:15 -0300

    created main

---
+++ RAD/ex.agenda_contatos/main.py
@@ -0,0 +1,4 @@
+'''crie uma classe chamada contato para representar um contato com informações como nome, sobrenome, email e telefone. Em seguida crie uma classe controleContatos que permite adicionar , buscar e remover contatos , agora utilizando txt para manipular os dados'''
+
+if __name__ == '__main__':
+    pass
