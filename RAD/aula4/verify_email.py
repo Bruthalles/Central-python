381d569692cf4c2ecf735dd74fc2889a35828d6d
Bruthalles <thallesgmkr@gmail.com>
2025-04-08 09:32:58 -0300

    update verify_email

--- RAD/aula4/verify_email.py
+++ RAD/aula4/verify_email.py
@@ -2,6 +2,5 @@ import re
 def verify_email(email):
     pattern = r"^\b\w+@\w+\.\w+\b$"
     if re.fullmatch(pattern,email):
-        print(f"\nEmail: '{email}' válido!")
-    else:
-        print("\nemail invalido")
+        return 0
+    else:return"\nEste formato não é um email válido" 
