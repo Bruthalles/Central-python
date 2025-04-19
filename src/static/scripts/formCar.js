c72440705bb0efab63a86002d6e3611e276f6ae6
Bruthalles <thallesgmkr@gmail.com>
2025-04-18 21:24:41 -0300

    debugando objeto carro

--- src/static/scripts/formCar.js
+++ src/static/scripts/formCar.js
@@ -1,24 +1,24 @@
-export function formCar(){
-    let container = document.getElementById("add-car-container");
-        container.innerHTML= `
+// export function formCar(){
+//     let container = document.getElementById("add-car-container");
+//         container.innerHTML= `
        
-            <label class="label">
-                <p class="p">Marca</p>    
-            </label>
-            <input id="marca" type="text" placeholder="Ex. Honda" required>
+//             <label class="label">
+//                 <p class="p">Marca</p>    
+//             </label>
+//             <input id="marca" type="text" placeholder="Ex. Honda" required>
 
-            <label class="label">
-                <p class="p">Modelo</p>    
-            </label>
-            <input id="modelo" type="text" placeholder="Ex. Civic" required>
+//             <label class="label">
+//                 <p class="p">Modelo</p>    
+//             </label>
+//             <input id="modelo" type="text" placeholder="Ex. Civic" required>
 
-            <label class="label">
-                <p class="p">Ano</p>    
-            </label>
-            <input id="ano"  type="text" placeholder="Ex. 2025" required>
+//             <label class="label">
+//                 <p class="p">Ano</p>    
+//             </label>
+//             <input id="ano"  type="text" placeholder="Ex. 2025" required>
 
-            <button class="guardar-carro" onclick="guardarCar()" type="submit">Guardar carro</button>
-       `;
+//             <button class="guardar-carro" onclick="guardarCar()" type="submit">Guardar carro</button>
+//        `;
         
-}
-window.formCar = formCar;
+// }
+// window.formCar = formCar;
